d757b49ebd41bfcbe23939e88e3e69242a312784
IvanPinna <ivanpinaarevalo@gmail.com>
2019-04-11 20:01:40 +0200

    Falta entrelazar los archivos

---
+++ binary.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+#define INF 2147483647
+
+using namespace std;
+
+int main(){
+  bool opt[8];
+  for(int i = 0; i < 3; ++i)
+    opt[i] = false;
+
+  while(contador != 3)
+  {
+    
+  }
+}
+
